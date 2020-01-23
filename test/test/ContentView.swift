8269bdaf45a44d8d74551b62a3980d3f3d14ddf8
oliverran <ranweiguang@zens-MacBook-Pro.local>
2020-01-23 16:00:38 +0000

    first commit

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 冉伟光 on 23/01/2020.
+//  Copyright © 2020 冉伟光. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
